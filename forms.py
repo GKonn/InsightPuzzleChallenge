4d9a46b2a3fbf94f7d1bdcb4e570f57bfee0d071
GKonn <31679844+GKonn@users.noreply.github.com>
2020-04-28 06:59:34 -0400

    READ finalthoughts.txt for more information

--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired, NumberRange
 
 class ItemForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
-    quantity = StringField('quantity', validators=[DataRequired()])
+    quantity = IntegerField('quantity', validators=[DataRequired(),NumberRange(min=1,max=None,message="quantity must be 1 or more")])
     description = StringField('description', validators=[DataRequired()])
 
